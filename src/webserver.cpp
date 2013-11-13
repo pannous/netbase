62f3d9b06ed7752ed254244ec4d30e22135aa8c0
Pannous <info@pannous.net>
2013-11-13 15:54:52 +0100

    ""

--- src/webserver.cpp
+++ src/webserver.cpp
@@ -114,7 +114,7 @@ int Service_Request(int conn) {
 	}
 	if (startsWith(q, "html/")) {
 			format = html;
-			if(!contains(q,"."))
+			if(!contains(q,".")&&!contains(q,":"))
 			verbosity=verbose;
 			q = q + 5;
 		}
