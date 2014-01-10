ac45aaffea4ea4ac55aa0214d1bf02a9ccb16692
Pannous <info@pannous.net>
2014-01-10 12:34:31 +0100

    x

--- import/babelnet/src/it/uniroma1/lcl/knowledge/graph/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Package including all classes related to
- * {@link it.uniroma1.lcl.knowledge.graph.KnowledgeGraph}s.
- */
-package it.uniroma1.lcl.knowledge.graph;
--- import/babelnet/src/it/uniroma1/lcl/knowledge/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * The root package of the Knowledge graph API. For more information start by
- * looking at the utility methods defined in 
- * {@link it.uniroma1.lcl.knowledge.graph.KnowledgeGraphUtils}.
- */
-package it.uniroma1.lcl.knowledge;
