ee6e10bf1ce63bb8d191b035f2656056df0c168d
Pannous <info@pannous.net>
2014-03-07 19:07:35 +0100

    ''

--- clear-shared-memory.sh
+++ clear-shared-memory.sh
@@ -1,24 +1,37 @@
-# allow bigger shared memory
 echo "All instances of netbase need to be stopped to clear memory"
 sudo killall netbase
 echo linux
-# sudo /sbin/sysctl -w kernel.shmmax=4294967296
-# sudo /sbin/sysctl -w kernel.shmall=4294967296
-#  32 GB !!!
-sudo /sbin/sysctl -w kernel.shmmax=34359738368 #32GB
-sudo /sbin/sysctl -w kernel.shmall=34359738368
 
-echo mac
-# sudo sysctl -w kern.sysv.shmmax=4294967296 #4GB
-# sudo sysctl -w kern.sysv.shmall=4294967296
-# sudo sysctl -w kern.sysv.shmmax=6442450944 #6GB
-# sudo sysctl -w kern.sysv.shmall=6442450944
-sudo sysctl -w kern.sysv.shmmax=8589934592 # 8GB
-sudo sysctl -w kern.sysv.shmall=8589934592
-# sudo sysctl -w kern.sysv.shmmax=34359738368 # 32GB
-# sudo sysctl -w kern.sysv.shmall=34359738368
+# shared memory limit
+export shmmax=2147483648 	# 2GB
+export shmmax=4294967296	# 4GB
+export shmmax=6442450944	# 6GB
+export shmmax=8589934592	# 8GB
+export shmmax=17179869184	# 16GB
+export shmmax=34359738368	# 32GB
+export shmmax=68719476736 	# 64GB
+
+# todo : use/set in netbase
+
+# allow bigger shared memory
+if [ $1 ] 
+	then
+		# echo "kernel.shmmax=$shmmax" >> /etc/sysctl.conf
+		# echo "kernel.shmall=$shmmax" >> /etc/sysctl.conf
+	if [ $APPLE ] 
+	then
+		sudo sysctl -w kern.sysv.shmmax=$shmmax
+		sudo sysctl -w kern.sysv.shmall=$shmmax
+	else 
+		sysctl -w kernel.shmall=$shmmax
+		sysctl -w kernel.shmmax=$shmmax
+		# sudo sysctl -w sys.kernel.shmmax=$shmmax
+		# sudo sysctl -w sys.kernel.shmmall=$shmmax
+	fi
+fi
 
 ipcclean
+
 sudo ipcrm -M "0x00069190"
 sudo ipcrm -M "0x00069191"
 sudo ipcrm -M "0x00069192"
