4518203d1c0fa41314369bffad27e44278d8f66d
Pannous <info@pannous.com>
2016-01-18 16:10:28 +0100

    ''

---
+++ import/wikidata/extract-labels.sh
@@ -0,0 +1,12 @@
+# wikidata-terms.de.nt
+gzcat wikidata-terms.nt.gz| grep "@de ." | \
+sed 's|http://www.wikidata.org/entity/||' | \
+sed 's|http://www.w3.org/2000/01/rdf-schema#||' | \
+sed 's|http://www.w3.org/2004/02/skos/core#||' | \
+sed 's|http://schema.org/||' > wikidata-terms.de.nt
+
+gzcat wikidata-terms.nt.gz| grep "@en ." | \
+sed 's|http://www.wikidata.org/entity/||' | \
+sed 's|http://www.w3.org/2000/01/rdf-schema#||' | \
+sed 's|http://www.w3.org/2004/02/skos/core#||' | \
+sed 's|http://schema.org/||' > wikidata-terms.en.nt
