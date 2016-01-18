0ff9cefa88604849308021f99c730397e12168e9
Pannous <info@pannous.com>
2016-01-18 15:07:14 +0100

    ''

--- import/wikidata/fetch_wikidata.sh
+++ import/wikidata/fetch_wikidata.sh
@@ -1,10 +1,9 @@
 DATE=20160104
-alias wget="wget -Nv"
-wget https://tools.wmflabs.org/wikidata-exports/rdf/exports/$DATE/wikidata-instances.nt.gz
-wget https://tools.wmflabs.org/wikidata-exports/rdf/exports/$DATE/wikidata-properties.nt.gz
-wget https://tools.wmflabs.org/wikidata-exports/rdf/exports/$DATE/wikidata-simple-statements.nt.gz
-wget https://tools.wmflabs.org/wikidata-exports/rdf/exports/$DATE/wikidata-sitelinks.nt.gz
-wget https://tools.wmflabs.org/wikidata-exports/rdf/exports/$DATE/wikidata-statements.nt.gz
-wget https://tools.wmflabs.org/wikidata-exports/rdf/exports/$DATE/wikidata-taxonomy.nt.gz
-wget https://tools.wmflabs.org/wikidata-exports/rdf/exports/$DATE/wikidata-terms.nt.gz
+wget -N https://tools.wmflabs.org/wikidata-exports/rdf/exports/$DATE/wikidata-instances.nt.gz
+wget -N https://tools.wmflabs.org/wikidata-exports/rdf/exports/$DATE/wikidata-properties.nt.gz
+wget -N https://tools.wmflabs.org/wikidata-exports/rdf/exports/$DATE/wikidata-simple-statements.nt.gz
+wget -N https://tools.wmflabs.org/wikidata-exports/rdf/exports/$DATE/wikidata-sitelinks.nt.gz
+wget -N https://tools.wmflabs.org/wikidata-exports/rdf/exports/$DATE/wikidata-statements.nt.gz
+wget -N https://tools.wmflabs.org/wikidata-exports/rdf/exports/$DATE/wikidata-taxonomy.nt.gz
+wget -N https://tools.wmflabs.org/wikidata-exports/rdf/exports/$DATE/wikidata-terms.nt.gz
 
