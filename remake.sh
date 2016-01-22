676fa98bdc8abb07954f2aea9802bf8f099ce7ee
Pannous <info@pannous.com>
2016-01-22 15:58:59 +0100

    ''

--- remake.sh
+++ remake.sh
@@ -1,1 +1,5 @@
-git pull && ./adjust.sh && make -j8 && screen -R netbase
+if [ !$APPLE ]; then rm src/webserver.cpp; git checkout src/webserver.cpp; fi
+git pull 
+./adjust.sh 
+make -j8 
+screen -R netbase ./server.sh :de
