119bae152684bb1e6c08b517bfee20b330f84e64
Pannous <info@pannous.net>
2018-04-20 11:59:07 +0200

    chaos-monkey.py

---
+++ chaos-monkey.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from extensions import *
+import urllib
+words=read_lines("/data/words/english.txt")
+words+=read_lines("/data/words/german.txt")
+
+def ran():
+	return (words[int(random()*len(words))]).replace(".","")
+
+url="http://localhost:8080/ee/"
+while 1:
+	# word=ran()+" "+ran()+" "+ran()
+	word=ran()+"+"+ran()+"+"+ran()
+	print(word)
+	curl(url+urlencode({'q':word}))
+	# curl(url+parse.quote(word))
